47a1e02890e30bd41d78436eb564b865193fc934
Juanantogg <juanantogg@outlook.com>
2018-08-08 00:56:32 -0500

    primer commit

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+var mapa = document.getElementById('mapa');
+
+var ubicacion = function () {
+	navigator.geolocation.getCurrentPosition(mostrar);
+}
+
+var mostrar = function () {
+	mapa.innerHTML =
+}
+
